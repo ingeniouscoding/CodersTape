d2f6742e351b19ffef321916071e69fe13023b0b
ingeniouscoding <dimchik_st@list.ru>
2021-12-15 01:09:10 +0300

    Extract navigation to file

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,21 +14,9 @@
 </head>
 
 <body>
-    <ul class="nav">
-        <li class="nav-item">
-            <a class="nav-link active" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="contact">Contact Us</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="about">About</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="customers">Customers</a>
-        </li>
-    </ul>
     <div class="container">
+        @include('nav')
+
         @yield('content')
     </div>
 </body>
