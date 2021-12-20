b13ca967c8f01dfbd5a0518c732ae2de88bcf15d
ingeniouscoding <dimchik_st@list.ru>
2021-12-21 01:12:43 +0300

    Api & Vue

---
+++ resources/js/components/MyButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button :type="type" class="my-button" v-text="test.name">My Button</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["type"],
+
+  data() {
+    return {
+      test: "",
+    };
+  },
+
+  mounted() {
+    console.log("Component mounted.");
+
+    axios.post("api/vue", {}).then((res) => {
+      this.test = res.data;
+    });
+  },
+};
+</script>
+
+<style>
+.my-button {
+  background-color: #333;
+  color: #fff;
+  padding: 10px 20px;
+}
+</style>
