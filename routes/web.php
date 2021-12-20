d6c2aebb9d47a694b6e1f11d07fb52379ff0bdf4
ingeniouscoding <dimchik_st@list.ru>
2021-12-20 20:56:28 +0300

    Add Auth and layout

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
-Route::view('/', 'welcome');
+Route::get('/', 'HomeController@index');
 Route::get('contact', 'ContactFormController@create');
 Route::post('contact', 'ContactFormController@store');
 
 Route::view('about', 'about');
 
 Route::resource('customers', 'CustomersController');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
